7a024afced0f55a33d31e0c2521d6f4a1db784ff
François Klingler <francois@fklingler.com>
2022-08-03 14:05:25 +0200

    feat(deck-detail): display cards in table, sort cards

--- src/app/components/deck-detail/deck-detail.component.ts
+++ src/app/components/deck-detail/deck-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Deck } from '../../storage';
 
+const colorOrder = ['W', 'U', 'B', 'R', 'G'];
+
 @Component({
     selector: 'deck-detail',
     templateUrl: './deck-detail.component.html',
@@ -9,4 +11,28 @@ import { Deck } from '../../storage';
 export class DeckDetailComponent {
     @Input()
     public deck?: Deck;
+
+    /**
+     * Returns filtered and sorted deck cards
+     */
+    public get displayedDeckCards(): Deck['cards'] {
+        if (!this.deck) {
+            return [];
+        }
+
+        return [...this.deck.cards]
+            .sort((a, b) => a.card.manaValue - b.card.manaValue)
+            .sort((a, b) => {
+                if (a.card.colors.length != b.card.colors.length) {
+                    return a.card.colors.length - b.card.colors.length;
+                }
+
+                return a.card.colors
+                        .map(c => Math.pow(2, colorOrder.indexOf(c) ?? 0))
+                        .reduce((acc, cur) => acc + cur, 0)
+                    - b.card.colors
+                        .map(c => Math.pow(2, colorOrder.indexOf(c) ?? 0))
+                        .reduce((acc, cur) => acc + cur, 0);
+            });
+    }
 }
